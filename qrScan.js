88cd6d32a57ff16b618deb10fe01be564016dcb9
webnetkz <8gg001@gmail.com>
2020-05-28 11:50:07 +0600

    update

--- qrScan.js
+++ qrScan.js
@@ -8,15 +8,15 @@ var app = new Vue({
   },
   mounted: function () {
     var self = this;
-    self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
-    self.scanner.addListener('scan', function (content, image) {
-      self.scans.unshift({ date: +(Date.now()), content: content });
+    	self.scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5 });
+    	self.scanner.addListener('scan', function (content, image) {
+      	self.scans.unshift({ date: +(Date.now()), content: content });
     });
     Instascan.Camera.getCameras().then(function (cameras) {
       self.cameras = cameras;
       if (cameras.length > 0) {
-        self.activeCameraId = cameras[0].id;
-        self.scanner.start(cameras[0]);
+        self.activeCameraId = cameras[1].id;
+        self.scanner.start(cameras[1]);
       } else {
         console.error('No cameras found.');
       }
